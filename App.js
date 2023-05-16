f7aaac443184940143dc1adc3e4c03cbf10b19e2
Thomas Van <thomascvan11@gmail.com>
2023-05-15 23:53:16 -0700

    adding web3 wallet connection functionality. in process.

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import TopBar from './components/TopBar';
 import ChatList from './components/ChatList';
+// import { Web3Modal } from '@web3modal/react-native';
+
+const projectId = '';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      {/* <Web3Modal projectId={projectId} /> */}
       <View style={styles.topBarContainer}>
         <TopBar />
       </View>
