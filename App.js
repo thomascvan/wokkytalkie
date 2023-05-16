99ba0009337bdaca09367eafe9b2fda3576f43d5
Thomas Van <thomascvan11@gmail.com>
2023-05-16 12:59:56 -0700

    added wallet connect button

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Pressable, Text } from 'react-native';
 import TopBar from './components/TopBar';
 import ChatList from './components/ChatList';
 import ChatView from './components/ChatView';
 
+import { Web3Modal, Web3Button, useWeb3Modal } from '@web3modal/react-native';
+
+const projectId = '';
+
+const providerMetadata = {
+  name: 'YOUR_PROJECT_NAME',
+  description: 'YOUR_PROJECT_DESCRIPTION',
+  url: 'https://your-project-website.com/',
+  icons: ['https://your-project-logo.com/'],
+};
+
 export default function App() {
   const [selectedChat, setSelectedChat] = useState(null);
+  const { isOpen, open, close, provider, isConnected } = useWeb3Modal();
 
   const handleChatClick = (chat) => {
     setSelectedChat(chat);
@@ -17,6 +29,11 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
+        {/* <Pressable onPress={open}>
+        <Text>{isConnected ? 'View Account' : 'Connect'}</Text>
+      </Pressable> */}
+      <Web3Button />
+      <Web3Modal projectId={projectId} providerMetadata={providerMetadata}  />
       <View style={styles.topBarContainer}>
         <TopBar />
       </View>
